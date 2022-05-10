11f835febc61676ee40e7af98f757ca25c6be94f
byodian <byj6696@gmail.com>
2022-05-10 09:51:20 +0800

    refactor the javascript selection sort

--- select-sort.js
+++ select-sort.js
@@ -1,19 +1,30 @@
+function findSmallest(arr) {
+  let smallestValue = arr[0];
+  let smallestIndex = 0;
+
+  for (let j = 1; j < arr.length; j++) {
+    if (arr[j] < smallestValue) {
+      smallestValue = arr[j];
+      smallestIndex = j;
+    }
+  }
+
+  return {
+    smallestValue,
+    smallestIndex
+  }
+}
+
 function sort(arr) {
   const sorted = [];
   const len = arr.length;
 
   for (let i = 0; i < len; i++) {
-    let smallestValue = arr[0];
-    let smallestIndex = 0;
-
-    for (let j = 1; j < arr.length; j++) {
-      if (arr[j] < smallestValue) {
-        smallestValue = arr[j];
-        smallestIndex = j;
-      }
-    }
+    const { smallestValue, smallestIndex } = findSmallest(arr);
 
     sorted.push(smallestValue);
+
+    // Change the length of the array to exclude the smallest value
     arr.splice(smallestIndex, 1);
   }
 
